495dffefd26b75489fc5a14c0a5198645e6ea0c2
Will Page <compenguy@gmail.com>
2021-06-02 22:20:25 -0700

    Major overhaul to actually connect to device

--- carduino.ino
+++ carduino.ino
@@ -1,112 +1,146 @@
-// ArduinoBLE - Version: 1.2.1
+// ArduinoBLE - Version: Latest
 #include <ArduinoBLE.h>
+
+// WiFiNINA - Version: Latest
+#include <WiFiNINA.h>
 
 // Arduino_LSM6DSOX - Version: Latest
 #include <Arduino_LSM6DSOX.h>
 
-const char BLE_SVC_UUID[] = "0000FFE0-0000-1000-8000-00805F9B34FB";
+//const char BLE_SVC_UUID[] = "0000FFE0-0000-1000-8000-00805F9B34FB";
 //const char BLE_SVC_UUID_SHORT[] = "ffe0";
-const char BLE_CHR_UUID[] = "0000FFE1-0000-1000-8000-00805F9B34FB";
-//const char BLE_CHR_UUID_SHORT[] = "ffe1";
+const char BLE_SVC_UUID_SHORT[] = "fff0";
+//const char BLE_CHR_UUID[] = "0000FFE1-0000-1000-8000-00805F9B34FB";
+const char BLE_CHR_UUID_SHORT[] = "ffe1";
+
+BLEDevice car;
+
+// The wifinina functions send the relevant commands to the nina module over SPI
+void wifinina_pin_mode(NinaPin pin, PinMode mode) {
+	WiFiDrv::pinMode(static_cast<uint8_t>(pin), static_cast<uint8_t>(mode));
+}
+
+void wifinina_digital_write(NinaPin pin, PinStatus value) {
+	WiFiDrv::digitalWrite(static_cast<uint8_t>(pin), static_cast<uint8_t>(value));
+}
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+	wifinina_pin_mode(LEDR, OUTPUT);
+	wifinina_pin_mode(LEDG, OUTPUT);
+	wifinina_pin_mode(LEDB, OUTPUT);
 
-  // begin initialization
-  if (!BLE.begin()) {
-    Serial.println("starting BLE failed!");
+	// NOTE: wifinina gpio access to LEDs is active-low
+	wifinina_digital_write(LEDR, LOW);
+	wifinina_digital_write(LEDB, HIGH);
+	wifinina_digital_write(LEDG, HIGH);
 
-    while (1);
-  }
-  if (!IMU.begin()) {
-    Serial.println("starting IMU failed!");
+	Serial.begin(9600);
+	while (!Serial);
 
-    while (1);
-  }
+	// begin initialization
+	if (!BLE.begin()) {
+		Serial.println("starting BLE failed!");
 
-  Serial.println("BLE Central scan callback");
+		while (1);
+	}
+	Serial.print("BLE interface initialized with address ");
+	Serial.println(BLE.address());
 
-  // assign event handlers for peripheral discovery, connection, and disconnection
-  BLE.setEventHandler(BLEDiscovered, bleCentralDiscoverHandler);
-  BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
-  BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
+	if (!IMU.begin()) {
+		Serial.println("starting IMU failed!");
 
-  // start scanning for peripherals with duplicates
-  BLE.scanForUuid(BLE_SVC_UUID, false);
+		while (1);
+	}
+
+	wifinina_digital_write(LEDR, HIGH);
+	wifinina_digital_write(LEDB, LOW);
+
+	Serial.println("BLE Central registering device callbacks");
+
+	// assign event handlers for peripheral discovery, connection, and disconnection
+	BLE.setEventHandler(BLEDiscovered, bleCentralDiscoverHandler);
+	BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+	BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
+
+	Serial.println("Scanning for supported device(s)");
+
+	// start scanning for peripherals with duplicates
+	BLE.scanForUuid(BLE_SVC_UUID_SHORT, false);
+
+	Serial.println("Setup completed.");
 }
 
 void loop() {
-  BLEDevice peripheral = BLE.available();
-  float x = 0.0f, y = 0.0f, z = 0.0f;
+	float x = 0.0f, y = 0.0f, z = 0.0f;
 
-  while (peripheral && peripheral.connected()) {
-    BLECharacteristic bleSerial = peripheral.characteristic(BLE_CHR_UUID);
-    if (!bleSerial || !bleSerial.canWrite()) {
-      Serial.println("Error attaching to the serial characteristic of the remote device.");
-      peripheral.disconnect();
-      break;
-    }
-    while (peripheral.connected()) {
-      if (IMU.gyroscopeAvailable()) {
-        float dx, dy, dz;
-        IMU.readGyroscope(dx, dy, dz);
-        x += dx;
-        y += dy;
-        z += dz;
-        // TODO: translate axial rotation into a vehicle command
-        // e.g. 0
-        bleSerial.writeValue((uint8_t)'f');
-        // TODO: add a button or some kind of signal to reset orientation, to counteract drift
-        // accelerometer is capable of detecting motion gestures - figure out how to make it respond to a
-        // 'tap' or 'shake' gesture.
-        // Additionally, calculate the axis along which 1G acceleration is being experienced
-        // that way is down to the controller, which can help 'zero' non-z rotation,
-        // for which no absolute reference exists
-      }
-    }
-  }
+	while (car && car.connected()) {
+		wifinina_digital_write(LEDB, HIGH);
+		wifinina_digital_write(LEDG, LOW);
+
+		Serial.println("Connected to remote device.");
+		if (!car.discoverAttributes()) {
+			Serial.println("Attribute discovery failed.");
+			car.disconnect();
+			break;
+		}
+		BLECharacteristic bleSerial = car.characteristic(BLE_CHR_UUID_SHORT);
+		if (!bleSerial || !bleSerial.canWrite()) {
+			Serial.println("Error attaching to the serial characteristic of the remote device.");
+			car.disconnect();
+			break;
+		}
+		while (car.connected()) {
+			if (IMU.gyroscopeAvailable()) {
+				float dx, dy, dz;
+				IMU.readGyroscope(dx, dy, dz);
+				x += dx;
+				y += dy;
+				z += dz;
+				// TODO: translate axial rotation into a vehicle command
+				// e.g. 0
+				bleSerial.writeValue((uint8_t)'f');
+				// TODO: add a button or some kind of signal to reset orientation, to counteract drift
+				// accelerometer is capable of detecting motion gestures - figure out how to make it respond to a
+				// 'tap' or 'shake' gesture.
+				// Additionally, calculate the axis along which 1G acceleration is being experienced
+				// that way is down to the controller, which can help 'zero' non-z rotation,
+				// for which no absolute reference exists
+			}
+		}
+	}
+	if (car) {
+		wifinina_digital_write(LEDB, LOW);
+		wifinina_digital_write(LEDG, HIGH);
+	} else {
+		Serial.println("Sleeping while waiting for a connection...");
+		delay(1000);
+		BLE.scanForUuid(BLE_SVC_UUID_SHORT, false);
+	}
 }
 
-void bleCentralDiscoverHandler(BLEDevice central) {
-  // discovered a peripheral
-  Serial.println("Discovered a supported peripheral");
-  Serial.println("-----------------------");
-  BLEDevice peripheral = BLE.available();
-
-  if (!peripheral.connect()) {
-    Serial.println("Connection failed.");
-    return;
-  }
-
-  Serial.println("Success!");
-  if (!peripheral.discoverAttributes()) {
-    Serial.println("Failed to discover device attributes. Disconnecting...");
-    peripheral.disconnect();
-    return;
-  }
-
-  BLECharacteristic serialCharacteristic = peripheral.characteristic(BLE_CHR_UUID);
-  if (!serialCharacteristic) {
-    Serial.println("Failed to locate required device characteristic. Disconnecting...");
-    peripheral.disconnect();
-    return;
-  }
-
+void bleCentralDiscoverHandler(BLEDevice peripheral) {
+	Serial.println("---------------------------");
+	Serial.println("Compatible peripheral found");
+	Serial.print("Name: ");
+	Serial.println(peripheral.localName());
+	Serial.print("Address: ");
+	Serial.println(peripheral.address());
+	Serial.print("Service UUID: ");
+	Serial.println(peripheral.advertisedServiceUuid());
+	if (peripheral.connect()) {
+		Serial.println("Connected.");
+		car = peripheral;
+	} else {
+		Serial.println("Connection failed.");
+	}
 }
 
 void blePeripheralConnectHandler(BLEDevice central) {
-  // central connected event handler
-  Serial.print("Connected event, central: ");
-  Serial.println(central.address());
-
-  BLE.stopScan();
+	Serial.println("Connected!");
+	BLE.stopScan();
 }
 
 void blePeripheralDisconnectHandler(BLEDevice central) {
-  // central disconnected event handler
-  Serial.print("Disconnected event, central: ");
-  Serial.println(central.address());
-
-  BLE.scanForUuid(BLE_SVC_UUID, false);
+	Serial.println("Disconnected!");
+	BLE.scanForUuid(BLE_SVC_UUID_SHORT, false);
 }
